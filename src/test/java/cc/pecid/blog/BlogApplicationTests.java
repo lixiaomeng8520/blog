10aa9984d43a7e9e8b56547700c237787fb7323f
lixiaomeng <lixiaomeng8520@163.com>
2022-12-14 09:23:31 +0800

    *

--- src/test/java/cc/pecid/blog/BlogApplicationTests.java
+++ src/test/java/cc/pecid/blog/BlogApplicationTests.java
@@ -1,13 +1,21 @@
 package cc.pecid.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import cc.pecid.blog.mapper.UserMapper;
 
 @SpringBootTest
 class BlogApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
 	@Test
 	void contextLoads() {
+        System.out.println(userMapper.getUserList().get(0));
+
 	}
 
 }
